bbdbe048a921962460014ca857d3168c514b03de
Shail <shailpatel67@gmail.com>
2020-01-12 13:07:51 -0500

    fix arrow menu having old inputs

--- src/input.js
+++ src/input.js
@@ -166,11 +166,20 @@ function updateSelectedArrow(){
 	selected_arrow.OUT = out.value;
 }
 
+function updateArrowMenu(){
+	if(selected_arrow === null || arrow_menu_drawn)
+        return;
+
+    if_.value = selected_arrow.IF;
+    out.value = selected_arrow.OUT; 
+}
+
 function drawArrowMenu(pos,if_text, out_text){
 	if(selected_arrow === null || arrow_menu_drawn)
 		return;
 
 	let w = Math.round(arrow_menu.offsetWidth/2);
+    updateArrowMenu();
 
 	arrow_menu.style.display = "block";	
 	arrow_menu.style.left = ((canvas.offsetLeft + pos.X + 15) - w) + "px";
